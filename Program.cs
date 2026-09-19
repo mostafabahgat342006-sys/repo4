7850f9cad5ee1502c097714ed7ecca3e8f090c20
mostafa bahgat <mostafabahgat342006@gmail.com>
2026-09-19 08:27:20 +0300

    Q2

--- Program.cs
+++ Program.cs
@@ -5,13 +5,24 @@ internal class Program
     class Book
     {
         private string password = "secret";
+        internal int copiesInStock = 5;
     }
 
     static void Main(string[] args)
     {
-        Book b = new Book();
+        Book b1 = new Book();
         // error brcause -> private allowe use it in class Book only
-        Console.WriteLine(b.password);
+        //Console.WriteLine(b.password);
+
+        Console.WriteLine("---------------------------------");
+
+        Book b2 = new Book();
+        // will compile if within the saame project
+        Console.WriteLine(b2.copiesInStock);
+
+        Console.WriteLine("---------------------------------");
+
+
 
 
 
