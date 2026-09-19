a68e82cc53ea7026b6c2694608777f7f73354b2a
mostafa bahgat <mostafabahgat342006@gmail.com>
2026-09-19 08:24:27 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -2,8 +2,16 @@
 
 internal class Program
 {
+    class Book
+    {
+        private string password = "secret";
+    }
+
     static void Main(string[] args)
     {
+        Book b = new Book();
+        // error brcause -> private allowe use it in class Book only
+        Console.WriteLine(b.password);
 
 
 
