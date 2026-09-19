a713023198a97736b096c2cecddc088a8ac578e9
mostafa bahgat <mostafabahgat342006@gmail.com>
2026-09-19 08:31:14 +0300

    Q4

--- Program.cs
+++ Program.cs
@@ -7,8 +7,18 @@ internal class Program
         private string password = "secret";
         internal int copiesInStock = 5;
         public string Title;
+       
+        public Genre Genre { get; set; }
+        
 
 
+    }
+
+    enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Science
     }
 
     static void Main(string[] args)
@@ -30,6 +40,14 @@ internal class Program
         b3.Title = "Clean Code";
 
         Console.WriteLine(b3.Title);
+
+        Console.WriteLine("---------------------------------");
+
+        Book b4 = new Book();
+
+        b4.Genre = Genre.Science;  
+
+        Console.WriteLine(b4.Genre);
 
         Console.WriteLine("---------------------------------");
 
